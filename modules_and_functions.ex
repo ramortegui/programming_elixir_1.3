e774e0d66fdcf80a1a03b5b53879723fcce67008
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-01-28 20:52:10 -0700

    Add solution for exercises 1,2,3 of Functions.

---
+++ modules_and_functions.ex
@@ -0,0 +1,16 @@
+# Modules and functions-1
+# Modules and functions-3
+
+defmodule Times do
+  def double(n), do: n * 2
+  def triple(n), do: n * 3
+  def quadruple(n), do: Times.double(n) * Times.double(n)
+end
+
+# Modules and functions-2
+# Compile the module calling iex or using iex>c "filename.ex"
+
+
+
+
+
