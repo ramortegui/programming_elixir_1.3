303233aaf1c8b187c38f76a346bf6fe50febfb62
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-04-03 12:31:42 -0600

    Solved Working with multiple process 3.

---
+++ working_with_multiple_process3.ex
@@ -0,0 +1,17 @@
+defmodule EixProcess do
+  import :timer, only: [sleep: 1]
+  def send_message(pid,msg) do
+    send pid,{:ok,"#{msg} done #{inspect pid}"}
+    exit(:boom)
+  end
+  def spawning do
+    pid = self()
+    spawn_link(EixProcess,:send_message,[ pid, "hello" ])
+    sleep(500)
+    receive do
+      { :ok,txt } -> IO.puts txt
+      after 500 ->
+          IO.puts "nothing happends"
+    end
+  end
+end
