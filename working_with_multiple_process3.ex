6760ed513297dd88585b766ad0771807a9872689
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-04-03 12:43:52 -0600

    Add commented code to solve working with multiple process 4 and 5.

--- working_with_multiple_process3.ex
+++ working_with_multiple_process3.ex
@@ -2,11 +2,13 @@ defmodule EixProcess do
   import :timer, only: [sleep: 1]
   def send_message(pid,msg) do
     send pid,{:ok,"#{msg} done #{inspect pid}"}
-    exit(:boom)
+    #exit(:boom)
+    raise("something wrong happend here")
   end
   def spawning do
     pid = self()
-    spawn_link(EixProcess,:send_message,[ pid, "hello" ])
+    #spawn_link(EixProcess,:send_message,[ pid, "hello" ])
+    spawn_monitor(EixProcess,:send_message,[ pid, "hello" ])
     sleep(500)
     receive do
       { :ok,txt } -> IO.puts txt
