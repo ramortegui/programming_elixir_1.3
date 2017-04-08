c76114a1fc257899ce449f7f4455b63d42c48b49
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-04-08 13:13:44 -0600

    Solves OTPServers-4.

--- OTP/stack/lib/stack.ex
+++ OTP/stack/lib/stack.ex
@@ -1,9 +1,19 @@
 defmodule Stack do
   use GenServer
+
+  def start_link( stack \\ [] ) do
+    GenServer.start_link(__MODULE__, stack, name: __MODULE__ )
+  end
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+  def push(val) do
+    GenServer.cast __MODULE__, {:push, val}
+  end
   def handle_call(:pop, _from, [h|t]) do
     {:reply, h, t }
   end
-  def handle_cast({:post,val}, stack) do
+  def handle_cast({:push,val}, stack) do
     { :noreply, [ val | stack ] }
   end
 end
