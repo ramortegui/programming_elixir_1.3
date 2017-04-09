2ef3810fb7b8b84e8b928be03ad923e40038006e
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-04-08 18:24:32 -0600

    Solves OTP-Supervisors-1

---
+++ OTP/stack_supervisor/stack/lib/stack/server.ex
@@ -0,0 +1,24 @@
+defmodule Stack.Server do
+  use GenServer
+
+  def start_link( stack \\ [] ) do
+    GenServer.start_link(__MODULE__, stack, name: __MODULE__ )
+  end
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+  def push(val) do
+    GenServer.cast __MODULE__, {:push, val}
+  end
+  def handle_call(:pop, _from, [h|t]) do
+    {:reply, h, t }
+  end
+  def handle_cast({:push,val}, stack) do
+    { :noreply, [ val | stack ] }
+  end
+
+  def terminate(reason,state) do
+    IO.puts "Error closing: #{inspect reason}"
+    IO.puts "Last state: #{inspect state}" 
+  end
+end
